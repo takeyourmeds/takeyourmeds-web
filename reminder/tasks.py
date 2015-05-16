e2e7944638ad83ff6ef87befaab7ea2fe7dadd82
David Szotten <davidszotten@gmail.com>
2015-05-16 15:36:39 +0100

    plug in real sending app

--- reminder/tasks.py
+++ reminder/tasks.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import
 from celery import shared_task
 from celery.utils.log import get_task_logger
 
+from telephony.actions import send_sms, make_call
+
 logger = get_task_logger(__name__)
 
 from .models import Reminder
@@ -10,5 +12,9 @@ from .models import Reminder
 @shared_task()
 def send_reminder_task(reminder_id):
     reminder = Reminder.objects.get(pk=reminder_id)
-    logger.debug('reminder sending: %s', reminder.pk)
-    # reminder.do_what_ross_says()
+    if reminder.message:
+        send_sms(reminder.phone_number, reminder.message)
+    elif reminder.url:
+            make_call(reminder.phone_number, reminder.url)
+    else:
+        logger.warn('Reminder %s has neither message nor url', reminder.pk)
