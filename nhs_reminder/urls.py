7b75602c523479583b132ad7c6ffd89d1f896c22
Ross Jones <ross@servercode.co.uk>
2015-05-17 11:29:33 +0100

    Implement base.html for the static pages
    Also adds /about

--- nhs_reminder/urls.py
+++ nhs_reminder/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
-from reminder.views import index, send, sent
+from reminder.views import send, sent
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url('^$', index),
+    url('^$', TemplateView.as_view(template_name='index.html')),
+    url('^about$', TemplateView.as_view(template_name='about.html')),
     url('^send/$', send, name='send'),
     url('^sent/$', sent, name='sent'),
     url('^telephony/', include("telephony.urls")),
