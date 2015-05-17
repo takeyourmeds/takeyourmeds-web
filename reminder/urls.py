79927ce434ff743431690f5c8363e6ecfc76d594
David Szotten <davidszotten@gmail.com>
2015-05-17 13:08:35 +0100

    list view

--- reminder/urls.py
+++ reminder/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from reminder.views import new_reminder
+from reminder.views import new_reminder, list_reminders
 
 urlpatterns = [
     url(r'^new/$', new_reminder, name="reminder_new" ),
+    url(r'^list/$', list_reminders, name="list-reminders" ),
 ]
