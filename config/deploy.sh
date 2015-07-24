ac7ee4e3ffdd7f1308d4b8e8419e5758adcf6125
Chris Lamb <chris@chris-lamb.co.uk>
2015-07-24 14:58:26 +0100

    Move deployment scripts from deployhq's interface to repo.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

---
+++ config/deploy.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+set -e
+
+Template () {
+	SOURCE="${1}"
+	TARGET="${2}"
+
+	# Could be a symlink
+	rm -f ${TARGET}
+
+	sed \
+		-e "s@__BASE_DIR__@${BASE_DIR}@g" \
+		"${SOURCE}" \
+		> sudo tee "${TARGET}"
+}
+
+
+if [ "$(id -u)" = 0 ]
+then
+	echo "${0}: should not be run as root"
+	exit 1
+fi
+
+BASE_DIR="$(readlink -f "$(dirname ${0})"./)"
+
+if [ ! -d "${BASE_DIR}" ]
+then
+	echo "${0}: target '${BASE_DIR}' does not exist"
+	exit 1
+fi
+
+cd "${BASE_DIR}"
+
+sudo apt-get install --yes \
+	python-virtualenv \
+	nginx \
+	gunicorn \
+	postgresql-9.3 \
+	libpq-dev \
+	python-dev
+
+# Stop services so we don't get race conditions
+sudo /etc/init.d/gunicorn stop
+sudo /etc/init.d/nginx stop
+
+# Virtualenv
+if [ ! -d "${BASE_DIR}/env" ]
+then
+	virtualenv env
+fi
+. env/bin/activate
+
+pip install -r requirements.txt
+pip install gunicorn psycopg2
+
+# Configure gunicorn. We want to use the initscripts from the Debian package,
+# but the Python interpreter from our virtualenv, so we take over
+# /usr/bin/gunicorn.
+sudo dpkg-divert --local --divert /usr/bin/gunicorn.dpkg-divert --rename /usr/bin/gunicorn
+sudo ln -sf ${BASE_DIR}/env/bin/gunicorn /usr/bin/gunicorn
+Template config/gunicorn /etc/gunicorn.d/takeyourmeds
+
+# Configure nginx
+sudo rm -f /etc/nginx/sites-enabled/default
+Template config/nginx /etc/nginx/sites-enabled/takeyourmeds
+
+# Migrations, etc.
+sudo -u postgres createuser takeyourmeds -SDR || true
+sudo -u postgres createdb -E UTF-8 -O takeyourmeds takeyourmeds || true
+python manage.py migrate --verbosity=2
+
+sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/nginx restart
