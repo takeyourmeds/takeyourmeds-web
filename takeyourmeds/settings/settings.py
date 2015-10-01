0dc77da3c107b25559412fe05375331f54c34613
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-01 17:36:31 +0200

    Gut .gitignore - all this stuff should be local to your git setup...
    .. otherwise you have to update 'n' projects when a new ".tmp" exists..

--- takeyourmeds/settings/settings.py
+++ takeyourmeds/settings/settings.py
@@ -101,7 +101,7 @@ LOGIN_REDIRECT_URL = "/"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite'),
     }
 }
 
