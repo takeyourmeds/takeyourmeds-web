b46cc11350148e31ebf6b55c173a4b4300eecb87
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-01 18:57:51 +0200

    Don't need namespacing now.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- takeyourmeds/settings/defaults/third_party.py
+++ takeyourmeds/settings/defaults/third_party.py
@@ -7,13 +7,11 @@ TWILIO_CONFIG = {
     #    "FROM_NUMBER": "+15005550006",
     #    "ROOT_URL": "http://127.0.0.1:8000/telephony/info"
     #},
-    "default": {
-        "ACCOUNT_SID": "XXX", # Your Twilio account SID
-        "AUTH_TOKEN": "XXX",  # Your twilio account SID
-        "FROM_NUMBER": "0000",  # Your twilio SMS-enabled phone number
-        # The root URL from where the Twilio service can pick
-        # up the TwilML because Ross is too lazy to work out the
-        # server name dynamically.
-        "ROOT_URL": "http://takeyourmeds.fixme/telephony/info"
-    }
+    "ACCOUNT_SID": "XXX", # Your Twilio account SID
+    "AUTH_TOKEN": "XXX",  # Your twilio account SID
+    "FROM_NUMBER": "0000",  # Your twilio SMS-enabled phone number
+    # The root URL from where the Twilio service can pick
+    # up the TwilML because Ross is too lazy to work out the
+    # server name dynamically.
+    "ROOT_URL": "http://takeyourmeds.fixme/telephony/info",
 }
