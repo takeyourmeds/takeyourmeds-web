daa48e802a6d6d1344ee288ccc540d6a9561c401
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-01 18:53:01 +0200

    Add a third-party module to hold this stuff
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- takeyourmeds/settings/defaults/__init__.py
+++ takeyourmeds/settings/defaults/__init__.py
@@ -10,11 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
+from third_party import *
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
