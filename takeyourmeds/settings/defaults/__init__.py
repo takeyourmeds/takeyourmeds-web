d40fe7740977687c21d484625ea177d7a08bb687
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-01 17:44:50 +0200

    o rly

--- takeyourmeds/settings/defaults/__init__.py
+++ takeyourmeds/settings/defaults/__init__.py
@@ -19,7 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
