2cdf9745c7b0ac40ba811eb576af9ef4c258b788
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-01 19:00:54 +0200

    Tidy this too.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- takeyourmeds/settings/defaults/third_party.py
+++ takeyourmeds/settings/defaults/third_party.py
@@ -1,17 +1,6 @@
 TWILIO_CONFIG = {
-    #"test": {
-    #    # You can retrieve these settings from
-    #    # https://www.twilio.com/user/account/developer-tools/test-credentials
-    #    "ACCOUNT_SID": "",
-    #    "AUTH_TOKEN": "",
-    #    "FROM_NUMBER": "+15005550006",
-    #    "ROOT_URL": "http://127.0.0.1:8000/telephony/info"
-    #},
-    "ACCOUNT_SID": "XXX", # Your Twilio account SID
-    "AUTH_TOKEN": "XXX",  # Your twilio account SID
-    "FROM_NUMBER": "0000",  # Your twilio SMS-enabled phone number
-    # The root URL from where the Twilio service can pick
-    # up the TwilML because Ross is too lazy to work out the
-    # server name dynamically.
-    "ROOT_URL": "http://takeyourmeds.fixme/telephony/info",
+    'ACCOUNT_SID': 'XXX',
+    'AUTH_TOKEN': 'XXX',
+    'FROM_NUMBER': '0000',
+    'ROOT_URL': 'http://takeyourmeds.fixm/telephony/info',
 }
