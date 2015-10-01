1b23ca7327d5414233222fdb26c1c1e88d0137a6
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-01 20:07:44 +0200

    Tidy imports and opening the tmpfile

--- takeyourmeds/telephony/views.py
+++ takeyourmeds/telephony/views.py
@@ -1,17 +1,13 @@
 import os
-import json
 
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-
-from . import actions
-
 
 def info(request, uuid):
     """
-    Return the Twillio ML that we generated when the user
-    called /call
+    Return the Twillio ML that we generated when the user called /call
     """
-    data = open(os.path.join("/tmp", uuid + ".xml")).read()
-    return HttpResponse(data, content_type="text/xml")
 
+    with open(os.path.join('/tmp', '%s.xml' % uuid)) as f:
+        data = f.read()
+
+    return HttpResponse(data, content_type='text/xml')
