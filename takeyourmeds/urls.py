e4a37e13b16f80d9bef2681e315670a669fce3bf
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-01 20:08:45 +0200

    Drop admin.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- takeyourmeds/urls.py
+++ takeyourmeds/urls.py
@@ -1,7 +1,6 @@
 from rest_framework import routers
 
 from django.conf.urls import include, url
-from django.contrib import admin
 from django.views.generic import TemplateView
 
 from reminder.api import ReminderViewSet
@@ -10,7 +9,6 @@ router = routers.DefaultRouter()
 router.register(r'reminders', ReminderViewSet)
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
     url('^$', TemplateView.as_view(template_name='index.html')),
     url('^about$', TemplateView.as_view(template_name='about.html')),
     url('^terms-and-conditions$', TemplateView.as_view(template_name='terms-and-conditions.html')),
