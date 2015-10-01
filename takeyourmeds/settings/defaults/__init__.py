40b295dee5ec6bc362492fa99a50ac53ea7e2827
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-01 19:03:45 +0200

    Disable i18n and just go with UTC and en-gb
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- takeyourmeds/settings/defaults/__init__.py
+++ takeyourmeds/settings/defaults/__init__.py
@@ -105,23 +105,11 @@ DATABASES = {
     }
 }
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.8/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
+USE_TZ = False
 TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
+USE_I18N = False
+USE_L10N = False
+LANGUAGE_CODE = 'en-gb'
 
 STATIC_URL = '/static/'
 
