136b748bdcced803868e1e9e80ad42fd5a0da04d
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-01 19:00:16 +0200

    Tidy, and these docs don't really help..
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- takeyourmeds/settings/roles/local.py
+++ takeyourmeds/settings/roles/local.py
@@ -1,13 +1,10 @@
 DEBUG = True
 
 TWILIO_CONFIG = {
-    "ACCOUNT_SID": "XXX", # Your Twilio account SID
-    "AUTH_TOKEN": "XXX",  # Your twilio account SID
-    "FROM_NUMBER": "0000",  # Your twilio SMS-enabled phone number
-    # The root URL from where the Twilio service can pick
-    # up the TwilML because Ross is too lazy to work out the
-    # server name dynamically.
-    "ROOT_URL": "http://127.0.0.1:8000/telephony/info",
+    'ACCOUNT_SID': 'XXX',
+    'AUTH_TOKEN': 'XXX',
+    'FROM_NUMBER': '0000',
+    'ROOT_URL': 'http://127.0.0.1:8000/telephony/info',
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
