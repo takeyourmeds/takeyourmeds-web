0232985c16772f70c0a96131f44c3561d70a0d52
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-01 17:33:57 +0200

    Very quick import tidy.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- takeyourmeds/urls.py
+++ takeyourmeds/urls.py
@@ -1,13 +1,13 @@
+from rest_framework import routers
+
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
-from rest_framework import routers
 
 from reminder.api import ReminderViewSet
 
 router = routers.DefaultRouter()
 router.register(r'reminders', ReminderViewSet)
-
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
