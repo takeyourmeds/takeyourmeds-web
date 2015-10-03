7475e6b945cf464bdcd459c2467779938bb972b9
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-03 12:10:41 +0200

    Move templates into app-specific dir
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- takeyourmeds/reminder/views.py
+++ takeyourmeds/reminder/views.py
@@ -3,20 +3,20 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def new_reminder(request):
-    return render(request, 'new_reminder.html')
+    return render(request, 'reminder/new_reminder.html')
 
 @login_required
 def list_reminders(request):
     reminders = request.user.reminder_set.all().order_by('-pk')
 
-    return render(request, 'list_reminders.html', {
+    return render(request, 'reminder/list_reminders.html', {
         'reminders': reminders,
     })
 
 def send(request):
     if request.method == 'POST':
         return redirect('sent')
-    return render(request, 'index.html')
+    return render(request, 'reminder/index.html')
 
 def sent(request):
-    return render(request, 'sent.html')
+    return render(request, 'reminder/sent.html')
