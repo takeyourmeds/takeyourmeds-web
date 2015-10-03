64acbc1306ab10d1421f773e4103d759d805c0c7
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-03 12:04:09 +0200

    Tidy reminder.urls
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- takeyourmeds/reminder/urls.py
+++ takeyourmeds/reminder/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 
-from .views import new_reminder, list_reminders
 from .api import trigger_now
+from .views import new_reminder, list_reminders
 
-urlpatterns = [
-    url(r'^new/$', new_reminder, name="reminder_new"),
-    url(r'^list/$', list_reminders, name="list-reminders" ),
-    url(r'^trigger/$', trigger_now, name="trigger_now"),
-]
+urlpatterns = (
+    url(r'^new/$', new_reminder,
+        name='reminder_new'),
+    url(r'^list/$', list_reminders,
+        name='list-reminders'),
+    url(r'^trigger/$', trigger_now,
+        name='trigger_now'),
+)
