88e52a5dfa39574401a5b159721f074069f6bad7
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-03 13:34:14 +0200

    Tidy reminder tests
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- takeyourmeds/reminder/tests.py
+++ takeyourmeds/reminder/tests.py
@@ -1,34 +1,19 @@
+import pytz
 import datetime
 
-import pytz
-from django.test import TestCase
-from django.contrib.auth.models import User
-
-from .models import Reminder, ReminderTime
-
+from takeyourmeds.utils.test import TestCase
 
 class TestCron(TestCase):
-
-    def setUp(self):
-        self.u = User(username='test')
-        self.u.save()
-
     def test_cron(self):
-
-        ten_min_ago = (
-            datetime.datetime.now(pytz.utc) -
+        ten_min_ago = datetime.datetime.now(pytz.utc) - \
             datetime.timedelta(minutes=10)
-        )
-        r = Reminder(
-            user=self.u,
-            message="test",
-            telnumber="123",
-        )
-        r.save()
 
-        rt = ReminderTime(
+        reminder = self.user.reminder_set.create(
+            message="test",
+            telnumber='123',
+        )
+
+        reminder.reminder_times.create(
             cronstring="* * * * *",
             last_run=ten_min_ago,
-            reminder=r,
         )
-        rt.save()
