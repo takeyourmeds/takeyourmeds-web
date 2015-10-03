b2e890a5da8b218360cc0310aac0752779fba761
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-03 19:27:11 +0200

    Add some smoketests for the reminders views
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- takeyourmeds/reminders/tests.py
+++ takeyourmeds/reminders/tests.py
@@ -3,6 +3,20 @@ import datetime
 
 from takeyourmeds.utils.test import TestCase
 
+class SmokeTest(TestCase):
+    def test_index(self):
+        self.assertHTTP200('reminders:index', login=True)
+
+    def test_create(self):
+        self.assertHTTP200('reminders:create', login=True)
+
+    def test_delete(self):
+        instance = self.user.reminders.create()
+
+        self.assert_(self.user.reminders.exists())
+        self.assertHTTP302('reminders:delete', instance.pk, login=True)
+        self.failIf(self.user.reminders.exists())
+
 class TestCron(TestCase):
     def test_cron(self):
         ten_min_ago = datetime.datetime.now(pytz.utc) - \
@@ -17,10 +31,3 @@ class TestCron(TestCase):
             cronstring="* * * * *",
             last_run=ten_min_ago,
         )
-
-    def test_delete(self):
-        instance = self.user.reminders.create()
-
-        self.assertHTTP302('reminders:delete', instance.pk, login=True)
-
-        self.failIf(self.user.reminders.exists())
