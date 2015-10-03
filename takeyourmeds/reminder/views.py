b35b29957bc29c9699ec534a372b7d67a1528ac9
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-03 12:09:25 +0200

    Tidy reminder.views

--- takeyourmeds/reminder/views.py
+++ takeyourmeds/reminder/views.py
@@ -1,25 +1,22 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
-
 @login_required
 def new_reminder(request):
     return render(request, 'new_reminder.html')
 
-
 @login_required
 def list_reminders(request):
-    user = request.user
-    return render(request, 'list_reminders.html', {
-        'reminders': user.reminder_set.all().order_by('-pk'),
-    })
+    reminders = request.user.reminder_set.all().order_by('-pk')
 
+    return render(request, 'list_reminders.html', {
+        'reminders': reminders,
+    })
 
 def send(request):
     if request.method == 'POST':
         return redirect('sent')
     return render(request, 'index.html')
-
 
 def sent(request):
     return render(request, 'sent.html')
