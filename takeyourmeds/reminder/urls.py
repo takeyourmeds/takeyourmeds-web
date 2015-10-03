c65045058cfdb6ddd0e237d312743fd46c0fb478
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-03 12:14:56 +0200

    Use urlpatterns for now
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- takeyourmeds/reminder/urls.py
+++ takeyourmeds/reminder/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import url
+from django.conf.urls import url, patterns
 
-from .api import trigger_now
-from .views import new_reminder, list_reminders
-
-urlpatterns = (
-    url(r'^new/$', new_reminder,
+urlpatterns = patterns('takeyourmeds.reminder.views',
+    url(r'^new/$', 'new_reminder',
         name='reminder_new'),
-    url(r'^list/$', list_reminders,
+    url(r'^list/$', 'list_reminders',
         name='list-reminders'),
-    url(r'^trigger/$', trigger_now,
+)
+
+urlpatterns += patterns('takeyourmeds.reminder.api',
+    url(r'^trigger/$', 'trigger_now',
         name='trigger_now'),
 )
