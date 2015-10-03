4b6fd00f996ba88d8c6ebb1e44992e5b5c2055e0
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-03 12:14:46 +0200

    Move static templates into their own dir

--- takeyourmeds/static/urls.py
+++ takeyourmeds/static/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('takeyourmeds.static.views',
-    url('^$', TemplateView.as_view(template_name='index.html'),
+    url('^$', TemplateView.as_view(template_name='static/index.html'),
         name='index'),
-    url('^about$', TemplateView.as_view(template_name='about.html'),
+    url('^about$', TemplateView.as_view(template_name='static/about.html'),
         name='about'),
-    url('^terms-and-conditions$', TemplateView.as_view(template_name='terms-and-conditions.html'),
+    url('^terms-and-conditions$', TemplateView.as_view(template_name='static/terms-and-conditions.html'),
         name='terms-and-conditions'),
-    url('^privacy-policy$', TemplateView.as_view(template_name='privacy-policy.html'),
+    url('^privacy-policy$', TemplateView.as_view(template_name='static/privacy-policy.html'),
         name='privacy-policy'),
 )
