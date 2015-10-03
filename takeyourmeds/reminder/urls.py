c43beda5be33543697152e4b5d2645086f99858d
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-03 14:22:00 +0200

    Can drop the suffix now that we have a namespace
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- takeyourmeds/reminder/urls.py
+++ takeyourmeds/reminder/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, patterns
 
 urlpatterns = patterns('takeyourmeds.reminder.views',
     url(r'^reminder/new/$', 'new_reminder',
-        name='reminder_new'),
+        name='new'),
     url(r'^reminder/list/$', 'list_reminders',
-        name='list-reminders'),
+        name='list'),
 )
