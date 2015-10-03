663934150ad53c9581d4b19d4574685650f6c062
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-03 16:52:15 +0200

    Tidy run_tasks.py

--- takeyourmeds/reminder/management/commands/run_tasks.py
+++ takeyourmeds/reminder/management/commands/run_tasks.py
@@ -1,10 +1,8 @@
 from django.core.management.base import BaseCommand
 
-from ..models import Reminder, ReminderTime
-
+from ..models import ReminderTime
 
 class Command(BaseCommand):
-
     def handle(self, **options):
         for reminder_time in ReminderTime.objects.all():
             if reminder_time.should_run():
