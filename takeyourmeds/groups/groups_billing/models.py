0668f0c7e4d6d6fcf33fc29abae3275f7b262b99
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-06 11:45:44 +0200

    Need to store the customer ident.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- takeyourmeds/groups/groups_billing/models.py
+++ takeyourmeds/groups/groups_billing/models.py
@@ -1,3 +1,5 @@
+import stripe
+
 from django.db import models
 
 from django_auto_one_to_one import AutoOneToOneModel
@@ -7,6 +9,8 @@ from ..models import Group
 from .plans import PLANS, FREE
 
 class Billing(AutoOneToOneModel(Group)):
+    stripe_customer_ident = models.CharField(unique=True, max_length=255)
+
     plan = models.IntegerField(
         choices=sorted((x.value, x.display) for x in PLANS.values()),
         default=FREE.value,
@@ -14,3 +18,14 @@ class Billing(AutoOneToOneModel(Group)):
 
     def get_plan_object(self):
         return PLANS[self.plan]
+
+    def get_stripe_customer(self):
+        return stripe.Customer.retrieve(self.stripe_customer_ident)
+
+    def sync(self):
+        customer = self.get_stripe_customer()
+
+        for x in customer.subscriptions.all(count=1).data:
+            self.plan = PlANS[x.plan.id].value
+
+        self.save(update_fields=('plan',))
