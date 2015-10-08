1f4aa111d9cc94b7f9ad3a9bb5aae8ccf31405a6
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-08 17:23:32 +0200

    Need to run under our virtualenv obviously
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- takeyourmeds/utils/setuptools.py
+++ takeyourmeds/utils/setuptools.py
@@ -3,4 +3,4 @@ import unittest
 
 class TestSuite(unittest.TestSuite):
     def run(self, *args, **kwargs):
-        assert os.system('takeyourmeds/manage.py test --verbosity=2') == 0
+        assert os.system('%(_)s takeyourmeds/manage.py test --verbosity=2' % os.environ) == 0
