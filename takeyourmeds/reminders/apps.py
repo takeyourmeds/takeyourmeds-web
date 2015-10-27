436a92f8cf8775c99aa9bc3f4099b4cbb689098c
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-27 23:04:10 +0000

    Define our reminders centrally

--- takeyourmeds/reminders/apps.py
+++ takeyourmeds/reminders/apps.py
@@ -3,5 +3,16 @@ from django.apps import AppConfig
 class RemindersConfig(AppConfig):
     name = 'takeyourmeds.reminders'
 
+    voice_reminders = (
+        (
+            'hi_mum_medication_reminder.mp3',
+            "Mum's medication reminder!",
+        ),
+        (
+            'its_fiona_mouthwash_reminder.mp3',
+            "Dental Hygienist",
+        ),
+    )
+
     def ready(self):
         from . import checks # NOQA
