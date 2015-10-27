3db3d9b827f3acaef2223fec75bc41a531a4f1ee
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-27 23:04:21 +0000

    Check that the MP3 files actually exist.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

---
+++ takeyourmeds/reminders/checks.py
@@ -0,0 +1,15 @@
+import os
+
+from django.core import checks
+from django.contrib.staticfiles.finders import find
+
+from .apps import RemindersConfig
+
+@checks.register()
+def voice_reminders_exist(app_configs, **kwargs):
+    for x, _ in RemindersConfig.voice_reminders:
+        if not find(os.path.join('mp3', x)):
+            yield checks.Error(
+                "Reminder recording missing: %r" % x,
+                id='takeyourmeds.reminders.E001',
+            )
