a529f1dd260d0e82f07a086f814766bf5be59c7f
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-28 18:25:10 +0000

    This is now account.User

--- takeyourmeds/reminders/models.py
+++ takeyourmeds/reminders/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from croniter import croniter
 
 class Reminder(models.Model):
-    user = models.ForeignKey('auth.User', related_name='reminders')
+    user = models.ForeignKey('account.User', related_name='reminders')
     message = models.CharField(blank=True, max_length=100)
     audiourl = models.CharField(blank=True, max_length=100)
     telnumber = models.CharField(max_length=200)
