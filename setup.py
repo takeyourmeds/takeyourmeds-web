212b0d7d5616437b4aacab142eb80f35a7124343
Chris Lamb <chris@chris-lamb.co.uk>
2015-11-02 11:39:49 +0000

    Install via setup.py, not dh_install

--- setup.py
+++ setup.py
@@ -5,6 +5,16 @@ import sys
 
 from setuptools import setup, find_packages
 
+def find_data_files(dirs):
+    result = []
+    for x in dirs:
+        for dirpath, _, filenames in os.walk(x):
+            result.append((
+                dirpath,
+                [os.path.join(dirpath, y) for y in filenames],
+            ))
+    return result
+
 if sys.argv[1:2] == ['test']:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'takeyourmeds.settings')
     from django.core.management import execute_from_command_line
@@ -14,4 +24,6 @@ setup(
     name='takeyourmeds',
     scripts=('takeyourmeds/manage.py',),
     packages=find_packages(),
+    zip_safe=False,
+    data_files=find_data_files(('static', 'templates')),
 )
