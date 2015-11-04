cb036ea87128b3ae144e9084104bd3dc24569671
Chris Lamb <chris@chris-lamb.co.uk>
2015-11-04 11:35:17 +0000

    Revert "Use django-setuptest now that 1.8 support merged"
    We're on 1.9 now, so another upstream patch needed.
    
    This reverts commit beecc7fd89e03d91739cc614c8939b6144f90533.
    
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import sys
 
 from setuptools import setup, find_packages
 
@@ -14,11 +15,15 @@ def find_data_files(dirs):
             ))
     return result
 
+if sys.argv[1:2] == ['test']:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'takeyourmeds.settings')
+    from django.core.management import execute_from_command_line
+    sys.exit(execute_from_command_line(sys.argv + ['--verbosity=2']))
+
 setup(
     name='takeyourmeds',
     scripts=('takeyourmeds/manage.py',),
     packages=find_packages(),
     zip_safe=False,
     data_files=find_data_files(('static', 'templates')),
-    test_suite='setuptest.setuptest.SetupTestSuite',
 )
--- test_settings.py
+++
@@ -1,1 +0,0 @@
-from takeyourmeds.settings import *
