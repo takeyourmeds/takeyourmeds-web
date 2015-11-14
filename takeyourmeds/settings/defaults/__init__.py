fb0618e0ace306bf79130b1d07205600d6bdabef
Chris Lamb <chris@chris-lamb.co.uk>
2015-11-14 19:40:07 +0200

    Specify this globally and use a nice code

--- takeyourmeds/settings/defaults/__init__.py
+++ takeyourmeds/settings/defaults/__init__.py
@@ -136,6 +136,6 @@ STRIPE_SECRET_KEY = 'overriden-in-production'
 STRIPE_PUBLISHABLE_KEY = 'overriden-in-production'
 
 TWILIO_ENABLED = True
-TWILIO_FROM = 'overriden-in-production'
+TWILIO_FROM = 'TakeYourMed' # 11-character limit
 TWILIO_AUTH_TOKEN = 'overriden-in-production'
 TWILIO_ACCOUNT_SID = 'overriden-in-production'
