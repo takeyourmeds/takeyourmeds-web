01035668c4c307387c8a615805188768ed508f10
Chris Lamb <chris@chris-lamb.co.uk>
2015-11-14 14:47:13 +0200

    Add some tests
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

---
+++ takeyourmeds/reminders/reminders_logging/tests.py
@@ -0,0 +1,31 @@
+from takeyourmeds.utils.test import TestCase
+
+from ..tasks import trigger_reminder
+
+from .enums import StateEnum
+
+class SmokeTest(TestCase):
+    def test_success(self):
+        instance = self.user.reminders.create(
+            message='test',
+        )
+        self.assertEqual(instance.log_entries.count(), 0)
+
+        trigger_reminder.delay(instance.pk)
+
+        entry = instance.log_entries.get()
+
+        self.assertEqual(entry.state, StateEnum.success)
+        self.assertEqual(entry.traceback, "")
+
+    def test_failure(self):
+        instance = self.user.reminders.create()
+
+        self.assertEqual(instance.log_entries.count(), 0)
+
+        trigger_reminder.delay(instance.pk)
+
+        entry = instance.log_entries.get()
+
+        self.assertEqual(entry.state, StateEnum.error)
+        self.assertNotEqual(entry.traceback, "")
