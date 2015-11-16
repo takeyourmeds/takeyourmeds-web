07f253d33c64923b367f4e56b135f41bdb9defc1
Chris Lamb <chris@chris-lamb.co.uk>
2015-11-16 17:01:19 +0200

    We don't pass this back anymore

--- takeyourmeds/telephony/tests.py
+++ takeyourmeds/telephony/tests.py
@@ -5,12 +5,12 @@ from .models import TwilioMLCallback
 
 class TestActions(TestCase):
     def test_call(self):
-        sid = make_call('+441324430099', 'https://api.twilio.com/cowbell.mp3')
+        sid = make_call('+441324430099', 'https://api.twilio.com/cowbell.mp3').sid
 
         self.assertEqual(len(sid), 34)
         self.assert_(TwilioMLCallback.objects.all()[0].get_callback_url())
 
     def test_sms(self):
-        sid = send_sms('+441324430099', "Hello!")
+        sid = send_sms('+441324430099', "Hello!").sid
 
         self.assertEqual(len(sid), 34)
