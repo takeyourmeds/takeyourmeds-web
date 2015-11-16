58dfe4c0b337a7a9fadbd72ff361116be3b26a90
Chris Lamb <chris@chris-lamb.co.uk>
2015-11-16 17:42:50 +0200

    40 chars

--- takeyourmeds/reminders/reminders_messages/urls.py
+++ takeyourmeds/reminders/reminders_messages/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = (
-    url(r'^_/status-callback/messages/(?P<ident>.+)$', views.status_callback,
+    url(r'^_/status-callback/messages/(?P<ident>\w{40})$', views.status_callback,
         name='status-callback'),
 )
