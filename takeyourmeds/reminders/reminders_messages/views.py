66f69188d9fe8c74c6d712655e7e6e8c3c03c4f7
Chris Lamb <chris@chris-lamb.co.uk>
2015-11-17 12:22:50 +0200

    Needs to be CSRF exempt

--- takeyourmeds/reminders/reminders_messages/views.py
+++ takeyourmeds/reminders/reminders_messages/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
 from .enums import StateEnum
 from .models import Message
 
+@csrf_exempt
 @require_POST
 def status_callback(request, ident):
     """
