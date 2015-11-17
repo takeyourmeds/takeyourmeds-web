feaa6fa7a80fe98b5a2e716b96f1de09bd0a58dc
Chris Lamb <chris@chris-lamb.co.uk>
2015-11-17 20:42:34 +0200

    Need to namespace URL

--- takeyourmeds/reminders/reminders_logs/urls.py
+++ takeyourmeds/reminders/reminders_logs/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = (
-    url(r'^reminders/(?P<slug>\w+)$', views.view,
+    url(r'^reminders/logs/(?P<slug>\w+)$', views.view,
         name='view'),
 )
