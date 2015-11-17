dd12a9fa01af061112a6b5e3b22d4856fbcbab31
Chris Lamb <chris@chris-lamb.co.uk>
2015-11-17 19:37:45 +0200

    Move further in.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- takeyourmeds/reminders/reminders_calls/enums.py
+++ takeyourmeds/reminders/reminders_calls/enums.py
@@ -1,9 +1,9 @@
 import enum
 
 class StateEnum(enum.IntEnum):
-    failed      =  0
-    dialing     = 10
-    answered    = 20
-    busy        = 30
-    no_answer   = 40
-    unknown     = 50
+    failed          =  0
+    dialing         = 10
+    answered        = 20
+    busy            = 30
+    no_answer       = 40
+    unknown         = 50
--- takeyourmeds/reminders/reminders_messages/enums.py
+++ takeyourmeds/reminders/reminders_messages/enums.py
@@ -1,8 +1,8 @@
 import enum
 
 class StateEnum(enum.IntEnum):
-    failed      =  0
-    sending     = 10
-    sent        = 20
-    delivered   = 30
-    unknown     = 40
+    failed          =  0
+    sending         = 10
+    sent            = 20
+    delivered       = 30
+    unknown         = 40
