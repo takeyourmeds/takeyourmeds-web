d05e387a02c7efbb527d121e6edb01d5e06317dc
Chris Lamb <chris@chris-lamb.co.uk>
2015-11-19 14:51:23 +0200

    Support passing an URL

--- takeyourmeds/utils/url.py
+++ takeyourmeds/utils/url.py
@@ -3,8 +3,8 @@ import urlparse
 from django.conf import settings
 from django.core.urlresolvers import reverse
 
-def reverse_absolute(urlconf, *args, **kwargs):
-    return urlparse.urljoin(
-        settings.SITE_URL,
-        reverse(urlconf, *args, **kwargs),
-    )
+def reverse_absolute(target, *args, **kwargs):
+    if not target.startswith('/'):
+        target = reverse(target, *args, **kwargs)
+
+    return urlparse.urljoin(settings.SITE_URL, target)
