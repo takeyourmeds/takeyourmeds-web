2d4c288192830b3c11e048c7cd389f3523228445
Chris Lamb <chris@chris-lamb.co.uk>
2015-11-22 18:21:25 +0200

    add missing urls

--- takeyourmeds/reminders/reminders_calls/urls.py
+++ takeyourmeds/reminders/reminders_calls/urls.py
@@ -1,8 +1,11 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from . import views
 
 urlpatterns = (
+    url(r'', include('takeyourmeds.reminders.reminders_calls.reminders_calls_audio.urls',
+        namespace='audio')),
+
     url(r'^_/twiml-callback/calls/(?P<ident>\w{40})$', views.twiml_callback,
         name='twiml-callback'),
     url(r'^_/gather-callback/calls/(?P<ident>\w{40})$', views.gather_callback,
