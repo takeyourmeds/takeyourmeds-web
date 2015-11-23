cfc6083c58d151934403ccf55444b122fec46604
Chris Lamb <chris@chris-lamb.co.uk>
2015-11-23 22:50:17 +0200

    Resolve here
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- takeyourmeds/utils/test.py
+++ takeyourmeds/utils/test.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.shortcuts import resolve_url
 from django.contrib.auth import get_user_model
-from django.core.urlresolvers import reverse
 
 User = get_user_model()
 
@@ -48,7 +47,7 @@ class TestCase(TestCase):
 
         return self.assertRedirects(
             response,
-            reverse(urlconf, args=args, kwargs=kwargs),
+            resolve_url(urlconf, *args, **kwargs),
             status_code,
             target_status_code,
         )
