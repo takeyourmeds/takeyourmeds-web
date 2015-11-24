58f403d6af919c08cfcf16a8607cc467a0f0c016
Chris Lamb <chris@chris-lamb.co.uk>
2015-11-24 10:18:01 +0200

    Must be ajax
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- takeyourmeds/utils/ajax.py
+++ takeyourmeds/utils/ajax.py
@@ -9,6 +9,10 @@ class ajax(object):
     def __call__(self, fn):
         @functools.wraps(fn)
         def wrapped(request, *args, **kwargs):
+            if not request.is_ajax():
+                return HttpResponseBadRequest()
+
+            return HttpResponseBadRequest()
             # @login_required returns a 30{1,2} redirect to some login page; we
             # want our Javascript applications to go down an "error" codepath
             # rather than try and parse the login page HTML, etc.
