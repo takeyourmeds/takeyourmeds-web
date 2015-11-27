48fb646c7378823848b68d60a1a04c621a6f593c
Chris Lamb <chris@chris-lamb.co.uk>
2015-11-27 21:43:39 +0200

    Associate access token on registration

--- takeyourmeds/registration/forms.py
+++ takeyourmeds/registration/forms.py
@@ -2,10 +2,12 @@ from django import forms
 
 from takeyourmeds.account.utils import validate_password
 
+from takeyourmeds.groups.models import AccessToken
 from takeyourmeds.account.models import User
 
 class RegistrationForm(forms.ModelForm):
     password = forms.CharField()
+    access_token = forms.CharField(required=False)
 
     class Meta:
         model = User
@@ -13,11 +15,21 @@ class RegistrationForm(forms.ModelForm):
             'email',
         )
 
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+
+        self.access_token = None
+
     def save(self):
         # Create user
         user = super(RegistrationForm, self).save(commit=False)
         user.set_password(self.cleaned_data['password'])
         user.save()
+
+        # Associate user with this access token if one was provided
+        if self.access_token is not None:
+            self.access_token.user = user
+            self.access_token.save()
 
         return user
 
@@ -34,3 +46,25 @@ class RegistrationForm(forms.ModelForm):
 
     def clean_password(self):
         return validate_password(self.cleaned_data.get('password'))
+
+    def clean_access_token(self):
+        val = self.cleaned_data['access_token'].strip()
+
+        if not val:
+            return val
+
+        try:
+            self.access_token = AccessToken.objects.get(
+                access_token__iexact=val,
+            )
+        except AccessToken.DoesNotExist:
+            raise forms.ValidationError(
+                "Could not recognise this access token."
+            )
+
+        if self.access_token.user_id is not None:
+            raise forms.ValidationError(
+                "This access token has already been used."
+            )
+
+        return self.access_token.val
