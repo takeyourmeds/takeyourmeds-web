fda59042cb132f19aa87e7a003b801c6be3f100f
Chris Lamb <chris@chris-lamb.co.uk>
2015-11-27 16:57:09 +0200

    First stab at a Token model
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- takeyourmeds/groups/models.py
+++ takeyourmeds/groups/models.py
@@ -1,4 +1,5 @@
 import datetime
+import functools
 
 from django.db import models
 from django.utils.crypto import get_random_string
@@ -32,4 +33,32 @@ class Group(models.Model):
             self.pk,
             self.name,
             self.slug,
+        )
+
+class Token(models.Model):
+    group = models.ForeignKey(Group, related_name='tokens')
+
+    token = models.CharField(
+        unique=True,
+        max_length=8,
+        default=functools.partial(get_random_string, 8, 'ACEFHKJMLNPRUTWVYX'),
+    )
+
+    user = models.OneToOneField(
+        'account.User',
+        null=True,
+        related_name='token',
+    )
+
+    created = models.DateTimeField(default=datetime.datetime.utcnow)
+
+    class Meta:
+        ordering = ('-created',)
+        get_latest_by = 'created'
+
+    def __unicode__(self):
+        return u"group_id=%r token=%r user_id=%s" % (
+            self.group_id,
+            self.token,
+            self.user_id,
         )
---
+++ takeyourmeds/groups/tests.py
@@ -0,0 +1,7 @@
+from takeyourmeds.utils.test import TestCase
+
+from .models import Group
+
+class SmokeTest(TestCase):
+    def test_token(self):
+        self.user.profile.group.tokens.create()
