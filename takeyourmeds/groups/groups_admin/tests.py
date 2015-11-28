69409f4b46f9e3a0d7aeca91bdd02333946f134d
Chris Lamb <chris@chris-lamb.co.uk>
2015-11-28 20:30:13 +0200

    Test groups:admin:view

--- takeyourmeds/groups/groups_admin/tests.py
+++ takeyourmeds/groups/groups_admin/tests.py
@@ -3,3 +3,11 @@ from takeyourmeds.utils.test import SuperuserTestCase
 class SmokeTest(SuperuserTestCase):
     def test_index(self):
         self.assertGET(200, 'groups:admin:index', login=True)
+
+    def test_view(self):
+        self.assertGET(
+            200,
+            'groups:admin:view',
+            self.user.profile.group_id,
+            login=True,
+        )
